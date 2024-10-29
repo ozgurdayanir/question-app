e6837006fad91d1d2c48a3526632d421dffc2dca
ozgurdayanir <ozgurdayanir@gmail.com>
2024-10-30 00:11:40 +0300

    timer.css added. timer sound added for time running out. Started styling project with css.

--- App/src/assets/components/timer/Timer.jsx
+++ App/src/assets/components/timer/Timer.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
+import './style.css';
+const Timer = ({ timeRemaining, currentQuestion }) => {
+  const [animationKey, setAnimationKey] = useState(0);
+  useEffect(() => {
+    setAnimationKey((prevKey) => prevKey + 1);
+  }, [currentQuestion]);
 
-const Timer = ({ timeRemaining }) => {
-  return <p>Time Remaining: {timeRemaining} seconds</p>;
+  const strokeColor = timeRemaining <= 10 ? 'red' : 'gray';
+  const clockSound = new Audio('./public/timer.mp3');
+
+  useEffect(() => {
+    clockSound.load();
+  }, []);
+
+  useEffect(() => {
+    if (timeRemaining === 11) {
+      clockSound.play().catch(error => {
+        console.log("Ses oynatılmadı, izinle ilgili bir sorun olabilir:", error);
+      });;
+    }
+  }, [timeRemaining]);
+
+  return (
+    <>
+      <div id='timer'>
+        <div id='time' style={{ color: strokeColor }}>{timeRemaining}</div>
+        <svg
+          key={animationKey}
+          className='time-indicator'
+          style={{ animationDuration: `${timeRemaining}s` }}>
+          <circle r="18" cx="20" cy="20" style={{ stroke: strokeColor }}></circle>
+        </svg>
+      </div>
+      <p>Time Remaining: {timeRemaining} seconds</p>;
+    </>
+  )
 };
 
 export default Timer;
